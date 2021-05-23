6e9bed2d2c3367b8354a31ec3f244de9f9d04752
Victor Kean <vickean@gmail.com>
2021-05-23 20:55:52 +0800

    Add serving images and pixi modules through express

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import express from 'express';
+import Path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { getLocalIP } from './network-utils.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = 8000;
 
 app.use(express.static('public'));
+app.use('/assets/', express.static('assets'));
+app.use(
+  '/pixi',
+  express.static(
+    Path.join(__dirname, '..', 'node_modules/pixi.js/dist/browser')
+  )
+);
+app.use(
+  '/pixi-graphics-extras',
+  express.static(
+    Path.join(
+      __dirname,
+      '..',
+      'node_modules/@pixi/graphics-extras/dist/browser'
+    )
+  )
+);
 
 app.get('/api', (req, res) => {
   res.send('this is an api page');
